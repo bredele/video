2339d0d2ee0044638e6cdbbf472f075c0daaada6
bredele <olivier.wietrich@gmail.com>
2014-04-27 15:53:43 -0600

    first draft local peers

---
+++ index.js
@@ -0,0 +1,42 @@
+
+/**
+ * Modules dependencies.
+ * @api private
+ */
+
+var attach = require('attach');
+var media = require('media');
+var wedge = require('wedge');
+
+
+/**
+ * video constructor.
+ * @api public
+ */
+
+module.exports = function(node, options) {
+
+  return function(peer) {
+    var data = wedge(peer.data, 'video');
+    var video = media({
+      video:true
+    });
+
+    // video should not be undefined
+    if(data.video) video.set(data);
+
+    video.on('stream', function(data, stream) {
+      peer.queue('_stream', stream);
+    });
+
+    // add stream always before offer
+    peer.on('create', function() {
+      peer.on('_stream', function(stream) {
+        peer.stream(stream);
+      });
+    });
+    
+    attach(video, node, options);
+  };
+
+};
